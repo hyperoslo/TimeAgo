9c1c961fdda512e477faaa58101792cf347be278
richardoti <richard@hyper.no>
2015-06-03 10:36:57 +0200

    New demo, refactoring
    New demo
    Code refactoring to conform to the code style guidelines
    Tests target in Demo removed
    Extension made public
    Podspec updated
    Fixes: #1, #2, #3, #5, #6, #8

---
+++ Demo/TimeAgoDemo/TimeAgoDemo/AgoViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import TimeAgo
+
+class AgoViewController: UIViewController {
+  
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var timePicker: UIDatePicker!
+  @IBOutlet weak var datePicker: UIDatePicker!
+  
+  @IBAction func pickerValueChanged() {
+    calculateTimeAgoString()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    dateLabel.text = "Just now"
+    timePicker.maximumDate = NSDate()
+    datePicker.maximumDate = NSDate();
+  }
+  
+  func calculateTimeAgoString() {
+    let calendar = NSCalendar.currentCalendar()
+    let dateComponents = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay, fromDate: datePicker.date)
+    let timeComponents = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: timePicker.date)
+    
+    let combinedComponents = NSDateComponents()
+    combinedComponents.year = dateComponents.year
+    combinedComponents.month = dateComponents.month
+    combinedComponents.day = dateComponents.day
+    combinedComponents.hour = timeComponents.hour
+    combinedComponents.minute = timeComponents.minute
+    combinedComponents.second = timeComponents.second
+    
+    let fullDate = calendar.dateFromComponents(combinedComponents)
+    dateLabel.text = fullDate?.timeAgo
+  }
+}
