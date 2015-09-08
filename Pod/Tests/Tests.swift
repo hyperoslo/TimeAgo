747628656ca096284fc8b2f347e92ed7fb1e3b9b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-08 11:55:43 +0200

    Adds the test target

--- Pod/Tests/Tests.swift
+++ Pod/Tests/Tests.swift
@@ -1,31 +1,35 @@
-import UIKit
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Ramon Gilabert Llop on 9/8/15.
+//  Copyright © 2015 Example. All rights reserved.
+//
+
 import XCTest
 
-@testable import Source
-
 class Tests: XCTestCase {
-
-  func testJustNow() {
-
-  }
-
-  func testSecondsAgo() {
-
-  }
-
-  func testMinutesAgo() {
-
-  }
-
-  func testHoursAgo() {
-
-  }
-
-  func testDayAgo() {
-
-  }
-
-  func testMonthAgo() {
-
-  }
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
 }
