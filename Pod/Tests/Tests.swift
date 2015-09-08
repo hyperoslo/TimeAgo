302913b991fc45ee2ecf983c22924b0445c6563a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-08 11:46:10 +0200

    Adds the methods

--- Pod/Tests/Tests.swift
+++ Pod/Tests/Tests.swift
@@ -1,6 +1,31 @@
 import UIKit
 import XCTest
 
+@testable import Source
+
 class Tests: XCTestCase {
 
+  func testJustNow() {
+
+  }
+
+  func testSecondsAgo() {
+
+  }
+
+  func testMinutesAgo() {
+
+  }
+
+  func testHoursAgo() {
+
+  }
+
+  func testDayAgo() {
+
+  }
+
+  func testMonthAgo() {
+
+  }
 }
