8ae3712e93fe5622430a1eb2d94605cd1ef581be
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-18 10:57:20 +0200

    Changes a bit the pod

--- Source/NSDate+TimeAgo.swift
+++ Source/NSDate+TimeAgo.swift
@@ -28,6 +28,40 @@ public extension NSDate {
     let hours = Int(round(Float(minutes) / 60.0))
 
     if seconds < 5 {
+      return NSDateTimeAgoLocalizedStrings("Just now")
+    } else if seconds < 60 {
+      return stringFromFormat("%%d %@seconds ago", withValue: seconds)
+    } else if seconds < 120 {
+      return NSDateTimeAgoLocalizedStrings("A minute ago")
+    } else if minutes < 60 {
+      return stringFromFormat("%%d %@minutes ago", withValue: minutes)
+    } else if minutes < 120 {
+      return NSDateTimeAgoLocalizedStrings("An hour ago")
+    } else if hours < 24 {
+      return stringFromFormat("%%d %@hours ago", withValue: hours)
+    } else if hours < 24 * 7 {
+      let formatter = NSDateFormatter()
+      formatter.dateFormat = String(format: "EEEE '%@' HH:mm", NSDateTimeAgoLocalizedStrings("at"))
+      return formatter.stringFromDate(self)
+    } else {
+      let formatter = NSDateFormatter()
+      formatter.dateFormat = String(format: "d MMM '%@' HH:mm", NSDateTimeAgoLocalizedStrings("at"))
+      return formatter.stringFromDate(self)
+    }
+  }
+
+  var timeSince: String {
+
+    if isInTheFuture(self) {
+      return NSDateTimeAgoLocalizedStrings("Now")
+    }
+
+    let now = NSDate()
+    let seconds = Int(fabs(timeIntervalSinceDate(now)))
+    let minutes = Int(round(Float(seconds) / 60.0))
+    let hours = Int(round(Float(minutes) / 60.0))
+
+    if seconds < 5 {
       return NSDateTimeAgoLocalizedStrings("Now")
     } else if seconds < 60 {
       return stringFromFormat("%%d %@sec", withValue: seconds)
@@ -37,11 +71,11 @@ public extension NSDate {
       return stringFromFormat("%%d %@h", withValue: hours)
     } else if hours < 24 * 7 {
       let formatter = NSDateFormatter()
-      formatter.dateFormat = String(format: "EEEE '%@' HH:mm", NSDateTimeAgoLocalizedStrings("at"))
+      formatter.dateFormat = String(format: "EE HH:mm")
       return formatter.stringFromDate(self)
     } else {
       let formatter = NSDateFormatter()
-      formatter.dateFormat = String(format: "d MMM '%@' HH:mm", NSDateTimeAgoLocalizedStrings("at"))
+      formatter.dateFormat = String(format: "d MMM HH:mm")
       return formatter.stringFromDate(self)
     }
   }
